8971f393ab0d68d05eace9d38c6de16365c2c8b2
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-24 19:40:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function getFormvalue() {
   const firstName = document.querySelector('input[name="fname"]').value;
   const lastName = document.querySelector('input[name="lname"]').value;
 
-  alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
+  alert(`${firstName}${lastName}`);
 }

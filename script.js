9f9c5bac463e7cbcbe2e586fdd5ed81ab5b2f70a
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-05-04 07:58:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function getFormvalue() {
-    //Write your code here
-	let firstName = document.querySelector('input[name = fname').value;
-	let lastName = document.querySelector('input[name = lname').value;
-	alert(firstName + " " + lastName);
-
+    var x=document.getElementById("form1");
+    console.log(x);
+    let form_info = ""
+    for (var i=0;i<x.length;i++) {
+        if (x.elements[i].value!='Submit') {
+			console.log(x.elements[i].value);
+            form_info += x.elements[i].name + ": " + x.elements[i].value + "\n"
+        }
+    }
+    alert(form_info.substring(0,form_info.length-1))
 }

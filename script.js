c7a000e3b102479d0282c306fcb362876ae7130a
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-05-04 08:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 function getFormvalue() {
-    var x=document.getElementById("form1");
-    console.log(x);
-    let form_info = ""
-    for (var i=0;i<x.length;i++) {
-        if (x.elements[i].value!='Submit') {
-			console.log(x.elements[i].value);
-            form_info += x.elements[i].name + ": " + x.elements[i].value + "\n"
-        }
-    }
-    alert(form_info.substring(0,form_info.length-1))
+    //Write your code here
+
+	var a = document.getElementById("firstName").value;
+	var b = document.getElementById("lastName").value;
+
+	alert(a+" "+b);
 }
